6397ef3fcebeaaa07aeb5597b735eb22f546ecaa
Sharjeel-Iftikhar <isharjeel63@gmail.com>
2023-01-28 01:32:02 +0500

    StatFull Widge and Custom Class

---
+++ ID_Project/spidey_card/lib/Quotes.dart
@@ -0,0 +1,6 @@
+class Quotes {
+  String text = '';
+  String author = '';
+
+  Quotes({required this.text, required this.author});
+}
--- ID_Project/spidey_card/lib/main.dart
+++ ID_Project/spidey_card/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Quotes.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -6,9 +7,18 @@ void main() {
   ));
 }
 
-class SpideyCard extends StatelessWidget {
+class SpideyCard extends StatefulWidget {
   const SpideyCard({super.key});
 
+  @override
+  State<SpideyCard> createState() => _SpideyCardState();
+}
+
+class _SpideyCardState extends State<SpideyCard> {
+  List<Quotes> quotes = [
+    Quotes(text: 'Never Lose Hope', author: 'Sheri'),
+    Quotes(text: 'Worries Stops where Faith begins', author: 'Sheri'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,82 +37,91 @@ class SpideyCard extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 15, 15, 15),
         elevation: 0.0,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/abc.jpeg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Color.fromARGB(255, 32, 32, 32),
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontFamily: 'Caveat',
-                fontSize: 18.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Sharjeel-Iftikhar',
-              style: TextStyle(
-                color: Color.fromARGB(255, 102, 29, 24),
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Caveat',
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'Spider\'s Tinkle Level',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontFamily: 'Caveat',
-                fontSize: 18.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '08',
-              style: TextStyle(
-                color: Color.fromARGB(255, 102, 29, 24),
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Caveat',
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                  size: 18.0,
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/abc.jpeg'),
+                  radius: 40.0,
                 ),
-                SizedBox(width: 10.0),
-                Text(
-                  'isharjeelIftikhar@gmail.com',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 102, 29, 24),
-                      letterSpacing: 1.0,
-                      fontSize: 20.0,
-                      fontFamily: 'Caveat'),
+              ),
+              Divider(
+                height: 60.0,
+                color: Color.fromARGB(255, 32, 32, 32),
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontFamily: 'Caveat',
+                  fontSize: 18.0,
                 ),
-              ],
-            ),
-          ],
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Sharjeel-Iftikhar',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 102, 29, 24),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Caveat',
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'Spider\'s Tinkle Level',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontFamily: 'Caveat',
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '08',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 102, 29, 24),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Caveat',
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                    size: 18.0,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'isharjeelIftikhar@gmail.com',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 102, 29, 24),
+                        letterSpacing: 1.0,
+                        fontSize: 20.0,
+                        fontFamily: 'Caveat'),
+                  ),
+                ],
+              ),
+              Container(
+                child: Column(
+                  children: quotes
+                      .map((Quot) => Text('${Quot.text} - ${Quot.author}'))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
